63df865a15a6d51a4e00a20798c1f546c514bbea
Tsubasahane <wozaiha@gmail.com>
2021-06-23 21:58:47 +0800

    Update ShiftTargetCastBarText.cs

--- Tweaks/UiAdjustment/ShiftTargetCastBarText.cs
+++ Tweaks/UiAdjustment/ShiftTargetCastBarText.cs
@@ -23,6 +23,8 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
         public class Config : TweakConfig {
             public int Offset = 8;
             public Alignment NameAlignment = Alignment.BottomRight;
+            public bool ShowCastTime;
+            public Alignment CastTimeAlignment = Alignment.TopLeft;
         }
         
         public Config LoadedConfig { get; private set; }
@@ -79,6 +81,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
             changed |= ImGuiExt.HorizontalAlignmentSelector("Ability Name Alignment", ref LoadedConfig.NameAlignment, VerticalAlignment.Bottom);
 
+<<<<<<< Updated upstream
             ImGui.PushFont(UiBuilder.IconFont);
             if (ImGui.Checkbox($"###{GetType().Name}_EnableCastTime",
                 ref PluginConfig.UiAdjustments.ShiftTargetCastBarText.EnableCastTime)) changed = true;
@@ -109,6 +112,14 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 ref PluginConfig.UiAdjustments.ShiftTargetCastBarText.CastTimeOffsetY)) changed = true;
             ImGui.SameLine();
             ImGui.Text("咏唱时间垂直偏移");
+=======
+            if (ImGui.Checkbox("Show Cast Time Left",ref LoadedConfig.ShowCastTime))
+            {
+                ImGui.Text("Cast Time Alignment");
+
+                changed |= ImGuiExt.HorizontalAlignmentSelector("Cast time left Alignment", ref LoadedConfig.CastTimeAlignment, VerticalAlignment.Top);
+            }
+>>>>>>> Stashed changes
         };
 
         public void OnFrameworkUpdate(Framework framework) {
@@ -125,11 +136,21 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             if (focusTargetInfo != null && (focusTargetInfo.Visible || reset))
                 HandleFocusTargetInfo(focusTargetInfo, reset);
 
+<<<<<<< Updated upstream
             var seperatedCastBar = framework.Gui.GetAddonByName("_TargetInfoCastBar", 1);
             if (seperatedCastBar != null && (seperatedCastBar.Visible || reset))
             {
                 HandleSeperatedCastBar(seperatedCastBar, reset);
                 if (!reset) return;
+=======
+            var focusTargetInfo = Common.GetUnitBase("_FocusTargetInfo");
+            if (focusTargetInfo != null && (focusTargetInfo->IsVisible || reset)) {
+                DoShift(focusTargetInfo->UldManager.NodeList[16]);
+                if (LoadedConfig.ShowCastTime)
+                {
+                    AddCastTimeTextNode(focusTargetInfo,(AtkTextNode*)focusTargetInfo->UldManager.NodeList[16],focusTargetInfo->UldManager.NodeList[16]->IsVisible);
+                }
+>>>>>>> Stashed changes
             }
 
             var mainTargetInfo = framework.Gui.GetAddonByName("_TargetInfo", 1);
@@ -137,6 +158,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 HandleMainTargetInfo(mainTargetInfo, reset);
         }
 
+<<<<<<< Updated upstream
         private unsafe void HandleSeperatedCastBar(Addon addon, bool reset = false)
         {
             var addonStruct = (AtkUnitBase*) addon.Address;
@@ -178,6 +200,26 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 return;
             var textNode = (AtkTextNode*) GetNodeById(addonStruct,5);
             AddCastTimeTextNode(addonStruct, textNode, textNode->AtkResNode.IsVisible);
+=======
+            var splitCastBar = Common.GetUnitBase("_TargetInfoCastBar");
+            if (splitCastBar != null && (splitCastBar->IsVisible || reset)) {
+                DoShift(splitCastBar->UldManager.NodeList[5]);
+                if (LoadedConfig.ShowCastTime)
+                {
+                    AddCastTimeTextNode(focusTargetInfo,(AtkTextNode*)focusTargetInfo->UldManager.NodeList[5],focusTargetInfo->UldManager.NodeList[5]->IsVisible);
+                }
+                if (!reset) return;
+            }
+
+            var mainTargetInfo = Common.GetUnitBase("_TargetInfo");
+            if (mainTargetInfo != null && (mainTargetInfo->IsVisible || reset)) {
+                DoShift(mainTargetInfo->UldManager.NodeList[44]);
+                if (LoadedConfig.ShowCastTime)
+                {
+                    AddCastTimeTextNode(focusTargetInfo,(AtkTextNode*)focusTargetInfo->UldManager.NodeList[44],focusTargetInfo->UldManager.NodeList[44]->IsVisible);
+                }
+            }
+>>>>>>> Stashed changes
         }
         
         private const int MinOffset = -24;
@@ -202,9 +244,74 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 UiHelper.SetPosition(node, 8, null);
                 UiHelper.SetSize(node, 188, null);
             }
-            
+
         }
 
+        private const int TargetCastNodeId = 99990002;
+
+        private unsafe void AddCastTimeTextNode(AtkUnitBase* unit, AtkTextNode* cloneTextNode, bool visible = false)
+        {
+            var textNode = (AtkTextNode*)GetNodeById(unit, TargetCastNodeId);
+            
+            if (textNode == null)
+            {
+                textNode = UiHelper.CloneNode(cloneTextNode);
+                textNode->AtkResNode.NodeID = TargetCastNodeId;
+                var newStrPtr = Common.Alloc(512);
+                textNode->NodeText.StringPtr = (byte*) newStrPtr;
+                textNode->NodeText.BufSize = 512;
+                UiHelper.SetText(textNode, "");
+                UiHelper.ExpandNodeList(unit, 1);
+                unit->UldManager.NodeList[unit->UldManager.NodeListCount++] = (AtkResNode*) textNode;
+
+                var nextNode = (AtkTextNode*)GetNodeById(unit, cloneTextNode->AtkResNode.NodeID-1);
+
+                textNode->AtkResNode.ParentNode = nextNode->AtkResNode.ParentNode;
+                textNode->AtkResNode.ChildNode = null;
+                textNode->AtkResNode.NextSiblingNode = (AtkResNode*) nextNode;
+                textNode->AtkResNode.PrevSiblingNode = null;
+                nextNode->AtkResNode.PrevSiblingNode = (AtkResNode*) textNode;
+                nextNode->AtkResNode.ParentNode->ChildCount += 1;
+            }
+
+            if (!visible)
+            {
+                UiHelper.Hide(textNode);
+            }
+            else
+            {
+                textNode->AlignmentFontType = (byte) LoadedConfig.CastTimeAlignment;
+                //UiHelper.SetPosition(textNode, PluginConfig.UiAdjustments.ShiftTargetCastBarText.CastTimeOffsetX,
+                //    PluginConfig.UiAdjustments.ShiftTargetCastBarText.CastTimeOffsetY);
+                //UiHelper.SetSize(textNode, cloneTextNode->AtkResNode.Width, cloneTextNode->AtkResNode.Height);
+                textNode->FontSize = 15;//(byte) PluginConfig.UiAdjustments.ShiftTargetCastBarText.CastTimeFontSize;
+                UiHelper.SetText(textNode, GetTargetCastTime().ToString("00.00"));
+                UiHelper.Show(textNode);
+            }
+
+        }
+
+        private float GetTargetCastTime()
+        {
+            if (PluginInterface.ClientState.LocalPlayer == null ||
+                PluginInterface.ClientState.Targets.CurrentTarget == null)
+                return 0;
+            var target = PluginInterface.ClientState.Targets.CurrentTarget;
+            if (target is Chara)
+            {
+                var castTime =
+                    Marshal.PtrToStructure<float>(target.Address +
+                                                  Dalamud.Game.ClientState.Structs.ActorOffsets.CurrentCastTime);
+                var totalCastTime =
+                    Marshal.PtrToStructure<float>(target.Address +
+                                                  Dalamud.Game.ClientState.Structs.ActorOffsets.TotalCastTime);
+                return totalCastTime - castTime;
+            }
+
+            return 0;
+        }
+
+<<<<<<< Updated upstream
         private const int TargetCastNodeId = 99990002;
 
         private unsafe void AddCastTimeTextNode(AtkUnitBase* unit, AtkTextNode* cloneTextNode, bool visible = false)
@@ -271,6 +378,8 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
         }
 
         
+=======
+>>>>>>> Stashed changes
         private static unsafe AtkResNode* GetNodeById(AtkUnitBase* compBase, uint id)
         {
             if (compBase == null) return null;
@@ -286,8 +395,14 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             return null;
         }
 
+<<<<<<< Updated upstream
         public override void Enable()
         {
+=======
+
+
+        public override void Enable() {
+>>>>>>> Stashed changes
             if (Enabled) return;
             LoadedConfig = LoadConfig<Config>() ?? PluginConfig.UiAdjustments.ShiftTargetCastBarText ?? new Config();
             PluginInterface.Framework.OnUpdateEvent += OnFrameworkUpdate;
